1223e4978180b4ad560f70cfbe67c2f97db0e110
Samuel Lee <sanelightspy@gmail.com>
2024-07-10 02:25:26 +0800

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from "./components/Navbar"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,10 +7,10 @@ function App() {
   return (
     <>
       <div>
-        <p>Hello</p>
+        <Navbar />
       </div>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="ml-10 mb-20 flex py-6">
+      <h1 className="text-2xl">Kwantum Institute</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
