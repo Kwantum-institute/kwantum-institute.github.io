4a25baa91fdb008b222bd77c5431ca0ca9e58f5c
Samuel Lee <sanelightspy@gmail.com>
2024-07-13 01:47:49 +0800

    Updated footer and colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import Hero from "./components/Hero";
 import Navbar3D from "./components/Navbar3D";
-import backgroundImage from "./assets/earth_background.jpg";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="relative w-full overflow-hidden flex flex-col items-center">
-      <div
-        className="absolute bg bg-repeat-y bg-[length:120%] -top-[200px] left-0 w-full h-[2000px] bg-center bg-fixed -z-10"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      />
+    <div className="relative w-full flex flex-col items-center">
+      <div className="bg"></div>
       <Navbar3D />
       <Hero />
-      <div className="mb-20 text-white">Made by Sambeige</div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+  return (
+    <div className="p-10 w-full h-full flex flex-col bg-black justify-center items-center">
+      <div className="text-white">
+        Kwantum Institute 2024 - All Rights Reserved.
+      </div>
+      <div className="flex flex-row gap-2">
+        <a
+          className="text-white underline"
+          href="https://github.com/Kwantum-institute/kwantum-institute.github.io"
+        >
+          Github
+        </a>
+        <a
+          className="text-white underline"
+          href="https://github.com/Kwantum-institute/kwantum-institute.github.io"
+        >
+          Instagram
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
