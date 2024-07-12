b65ad5430136d236347012cc5968b31be8f978f2
Samuel Lee <sanelightspy@gmail.com>
2024-07-13 02:54:01 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <div className="p-10 w-full h-full flex flex-col bg-black justify-center items-center">
-      <div className="text-white">
+      <p className="text-white text-center">
         Kwantum Institute 2024 - All Rights Reserved.
-      </div>
+      </p>
       <div className="flex flex-row gap-2">
         <a
           className="text-white underline"
