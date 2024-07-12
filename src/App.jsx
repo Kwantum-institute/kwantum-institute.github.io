74b505486635e506fe184af3f6fc2e64753a7461
Samuel Lee <sanelightspy@gmail.com>
2024-07-12 13:47:20 +0800

    Clean up code

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,14 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="relative w-full overflow-hidden">
-        <div
-          className="bg bg-repeat-y absolute bg-[length:120%] -top-[200px] left-0 w-full h-[2000px] bg-center bg-fixed -z-10 saturate-0 contrast-[0.1]"
-          style={{ backgroundImage: `url(${backgroundImage})` }}
-        />
-        <Navbar3D />
-      </div>
+    <div className="relative w-full overflow-hidden">
+      <div
+        className="absolute bg bg-repeat-y bg-[length:120%] -top-[200px] left-0 w-full h-[2000px] bg-center bg-fixed -z-10 saturate-0 contrast-[0.1]"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      />
+      <Navbar3D />
       <Hero />
-    </>
+    </div>
   );
 }
 
--- src/components/Navbar3D.jsx
+++ src/components/Navbar3D.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import "./Navbar3D.css";
 
 const Navbar3D = () => {
+  const navLinks = [
+    { text: "Home", href: "#", index: 5 },
+    { text: "About", href: "#", index: 4 },
+    { text: "Science", href: "#", index: 3 },
+    { text: "Technology", href: "#", index: 2 },
+    { text: "About", href: "#", index: 1 },
+  ];
+
   return (
-    <div className="mt-[250px] lg:mt-[420px] mb-[100px] lg:mb-[200px] lg:ml-0 flex flex-col items-center justify-center gap-20">
+    <div className="flex flex-col items-center justify-center gap-20 mt-[250px] lg:mt-[420px] mb-[100px] lg:mb-[200px] lg:ml-0">
       <div className="relative lg:left-[100px]">
         <div className="absolute bottom-[370px] -left-[55px] text-center lg:text-left lg:-left-3 text-6xl lg:text-[150px] lg:leading-[150px] lg:bottom-[100px] text-3d">
           Kwantum Institute
@@ -11,33 +19,27 @@ const Navbar3D = () => {
         <div className="hidden lg:inline lg:-left-[250px] lg:-bottom-[60px] text-xl lg:text-[40px] lg:leading-[50px] absolute text-3d-s">
           We
           <br />
-          educate <br />
-          others on <br />
-          various <br />
-          scientific <br />
+          educate
+          <br />
+          others on
+          <br />
+          various
+          <br />
+          scientific
+          <br />
           disciplines
         </div>
         <ul className="flex flex-col relative left-3 lg:flex-row navbar-3d items-center justify-center">
-          <li style={{ "--i": 5 }}>
-            <a href="#">
-              <span>
-                <div>K</div>
-              </span>
-              Home
-            </a>
-          </li>
-          <li style={{ "--i": 4 }}>
-            <a href="#">About</a>
-          </li>
-          <li style={{ "--i": 3 }}>
-            <a href="#">Science</a>
-          </li>
-          <li style={{ "--i": 2 }}>
-            <a href="#">Technology</a>
-          </li>
-          <li style={{ "--i": 1 }}>
-            <a href="#">About</a>
-          </li>
+          {navLinks.map(({ text, href, index }) => (
+            <li key={index} style={{ "--i": index }}>
+              <a href={href}>
+                <span>
+                  <div>{text[0]}</div>
+                </span>
+                {text}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
