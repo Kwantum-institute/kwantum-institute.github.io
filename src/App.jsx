b652828e2e9e58eef9d1e299b93ab02f4eb532ae
Samuel Lee <sanelightspy@gmail.com>
2024-07-13 00:11:29 +0800

    Added book

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import backgroundImage from "./assets/reel.png";
 
 function App() {
   return (
-    <div className="bg-black relative w-full overflow-hidden">
+    <div className="bg-black relative w-full overflow-hidden flex flex-col items-center">
       <div
         className="hidden absolute bg bg-repeat-y bg-[length:120%] -top-[200px] left-0 w-full h-[2000px] bg-center bg-fixed -z-10"
         style={{ backgroundImage: `url(${backgroundImage})` }}
       />
       <Navbar3D />
       <Hero />
+      <div className="mb-20 text-white">Made by Sambeige</div>
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
 const Hero = () => {
   return (
-    <div>
-      <div className="pt-10 bg-blue-950 h-[1000px] rounded-[200px]">
-        <h1 className="mx-10 text-3d-s skew-y-0 text-center text-6xl text-white">
+    <div className="hidden lg:flex w-3/4 mt-10 mb-[200px] items-center">
+      <div className="pt-10 w-2/3 bg-white h-[1000px] skew-y-[-15deg] border-8">
+        <h1 className="mx-20 text-center text-6xl text-white text-black font-black">
           Our Top Picks of the Day
         </h1>
       </div>
+      <div className="pt-10 w-2/3 bg-white h-[1000px] skew-y-[15deg] border-8"></div>
     </div>
   );
 };
