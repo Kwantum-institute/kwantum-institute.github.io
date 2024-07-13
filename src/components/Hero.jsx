75f0b0bd4d79ec5eb12665a170d1379bc5833f1f
Samuel Lee <sanelightspy@gmail.com>
2024-07-14 01:35:38 +0800

    Added thumbnail aspect ratio, improved responsive design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
-    <div className="flex w-3/4 mt-10 mb-[200px] items-center">
-      <div className="flex flex-col gap-10 w-full pt-10 lg:w-2/3 bg-gray-700 h-[2000px] lg:skew-y-[-15deg] border-8">
+    <div className="flex sm:w-3/4 mt-10 mb-[200px] items-center">
+      <div className="flex flex-col gap-10 w-full pt-10 lg:w-2/3 bg-gray-700 h-[2000px] lg:skew-y-[-15deg] sm:border-8">
         <h1 className="mx-10 lg:mx-20 text-center text-5xl lg:text-6xl text-3d-s skew-y-0 font-black">
           Our Top Picks of the Day
         </h1>
