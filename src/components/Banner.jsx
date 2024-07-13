96e3f2d8b40d74f15ce69fddea72186f3f4c7d19
Samuel Lee <sanelightspy@gmail.com>
2024-07-14 03:14:22 +0800

    Re-added banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 const Banner = () => {
   return (
-    <div className="hidden z-10 fixed bottom-2 right-2 p-5 bg-[#faeed0] h-[75px] gap-3 justify-center items-center rounded-xl opacity-50 hover:opacity-100 duration-[500ms]">
+    <div className="hidden z-10 lg:flex flex-row fixed bottom-2 right-2 p-5 bg-[#faeed0] h-[75px] gap-3 justify-center items-center rounded-xl opacity-50 hover:opacity-100 duration-[500ms]">
       <p className="text-3xl">❗️ </p>
       <p className="">
         We are looking for contributors,
