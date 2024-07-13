4eb22ab32fb2584b98e0df917ec73aa0b7760d4e
Samuel Lee <sanelightspy@gmail.com>
2024-07-13 19:00:12 +0800

    Routing fix 5?

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
